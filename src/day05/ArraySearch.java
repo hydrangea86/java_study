c177ad981edf3695a51903a20b962d6b32e94405
gkaan <gkaan@daum.net>
2021-09-29 15:53:36 +0900

    [21.09.29] #java Array Delete, insert, search

---
+++ src/day05/ArraySearch.java
@@ -0,0 +1,32 @@
+package day05;
+
+import java.util.Scanner;
+
+public class ArraySearch {
+
+    public static void main(String[] args) {
+
+        String[] foods = {"치킨", "피자", "삼겹살", "족발", "파스타"};
+
+        // 수정
+        //foods[0] = "양념치킨";
+
+        Scanner sc = new Scanner(System.in);
+
+        //순차 탐색 알고리즘
+        System.out.println("찾는 음식: ");
+        String target = sc.next();
+        sc.close();
+
+        int index = -1; //찾은 인덱스 (-1 초기값)
+        for (int i = 0; i < foods.length; i++) {
+            if (target.equals(foods[i])) {
+                index = i;
+                break;
+            }
+        }
+        boolean result = index != -1;
+        System.out.println("음식 탐색 성공" + result);
+        System.out.println("찾은 음식의 인덱스:" + index);
+    }
+}
